aea4001b4cc4232c5774a5394bccb1ceb0fc8357
Salim ibn Usmon <salimibnusmon@gmail.com>
2019-04-17 10:49:58 +0500

    blank layout installed

---
+++ backend/assets/DashboardAsset.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class DashboardAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        'bower_components/font-awesome/css/font-awesome.min.css',
+        'bower_components/Ionicons/css/ionicons.min.css',
+        'dist/css/AdminLTE.min.css',
+        'dist/css/skins/_all-skins.min.css',
+    ];
+    public $js = [
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'bower_components/jquery-slimscroll/jquery.slimscroll.min.js',
+        'bower_components/fastclick/lib/fastclick.js',
+        'dist/js/adminlte.min.js',
+        'dist/js/demo.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        // 'yii\bootstrap\BootstrapAsset',
+    ];
+}
--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -15,6 +15,7 @@ class SiteController extends Controller
     /**
      * {@inheritdoc}
      */
+    public $layout = 'dashboard';
     public function behaviors()
     {
         return [
